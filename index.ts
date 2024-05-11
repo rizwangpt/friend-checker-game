c04ceb134d03ae7c7cc014baa32cfa4e3a700ce8
rizwangpt <contactwithrizwan@gmail.com>
2024-05-11 17:33:13 +0500

    push my game to Github

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// FRIEND CHECKER GAME !!!
+
+let isFriend = await inquirer.prompt([{
+    name: "name",
+    type: "string",
+    message: "Enter your Friend Name:"
+}])
+if (isFriend.name === "Ali" || isFriend.name ==="Saad"){
+    console.log(`${isFriend.name} is your Friend`)
+} else {
+    console.log(`${isFriend.name} is not your Friend`)
+}
+
+
+
+
